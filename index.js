3fcd7f88a6b82caa0cd41f2e1e3761d62b3bad25
Chris Pang <chris_pang429@outlook.com>
2017-11-08 02:29:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const currentUser = 'Chris';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = 'Welcome';
