64c6f789f41d431e687009857e0c91eeeb017040
Chris Pang <chris_pang429@outlook.com>
2017-11-08 02:25:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Chris';
 
-const welcomeMessage = 'Welcome to Flatbook, ${currrentUser}';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
